3c8708d52d94b026a739a3989c99b54b63bc16fa
Documenter.jl <documenter@juliadocs.github.io>
2021-12-22 13:40:20 +0000

    build based on 41324ff

--- versions.js
+++ versions.js
@@ -1,10 +1,8 @@
-var DOC_VERSIONS = [
-  "stable",
-  "v1.2",
-  "v1.1",
-  "v0.3",
-  "v0.2",
-  "dev",
-];
-var DOCUMENTER_NEWEST = "1.2";
-var DOCUMENTER_STABLE = "stable";
+[
+"version": "stable", "title": "stable", "aliases": )["1",".","2"]},
+"version": "v1.2", "title": "v1.2", "aliases": )["1",".","2"]},
+"version": "v1.1", "title": "v1.1", "aliases": )["1",".","1"]},
+"version": "v0.3", "title": "v0.3", "aliases": )["v","0",".","3",".","0"]},
+"version": "v0.2", "title": "v0.2", "aliases": )["v","0",".","2",".","0"]},
+"version": "dev", "title": "dev", "aliases": )[]}
+]
